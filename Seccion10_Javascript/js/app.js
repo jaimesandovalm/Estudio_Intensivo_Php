3f78198868bd03dca0eddccc71140fa344265983
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-24 17:27:08 -0300

    Primeras clases

---
+++ Seccion10_Javascript/js/app.js
@@ -0,0 +1,50 @@
+
+//pimer codigo
+alert("hola mundo")
+
+//variables
+
+var NombreVriable = 'Audifonos'
+
+/**Inicalizar multiples variables */
+
+var Producto = 'Computador',
+    Disponible = true,
+    Categoria = 'Computadoras';
+
+/**Estilos para las variables */
+var nombre_producto, /**undescore */
+    nombreProducto, /**Camelcase */
+    NombreProducto;/**Pascal Case */
+
+/**Variables con let */
+
+let NombreVariable
+
+/**Variables con const */
+
+const Pi = 3.1416
+
+/**Numero y Operadores */
+
+let n1 = 1,
+    n2 = 2;
+
+console.log(n1 + n2)
+console.log(n1 - n2)
+console.log(n1 * n2)
+console.log(n1 / n2)
+console.log(n1 % n2)
+
+/**Metodo Math */
+
+let resultado = Math.PI /**VALOR DE PI */
+let resultado2 = Math.round(2.2) /**Redondea un numero */
+let resultado3 = Math.ceil(2.2)/**Siempre redondea hacia arriba */
+let resultado4 = Math.floor(2.5)/**Siempre redondea hacia abajo */
+let resultado5 = Math.sqrt(16)/**Sacar la raiz cuadrada */
+let reusltado6 = Math.abs(-200)/**Transforma el valor a positivo */
+let resultado7 = Math.min(3, 5, 1, 8)/**Selecciona el valor minimo */
+let resultado8 = Math.max(3, 5, 1, 8)/**Selecciona el valor maximo */
+
+console.log(resultado)
