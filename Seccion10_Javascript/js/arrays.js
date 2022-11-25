aed7f61c7bb0da69701feed915ba60b040162c46
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-25 12:55:39 -0300

    Metodos para Arrays

--- Seccion10_Javascript/js/arrays.js
+++ Seccion10_Javascript/js/arrays.js
@@ -2,4 +2,30 @@
 
 const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-console.log(numeros)
+
+/**Agregar un elemento al array */
+numeros[10] = 10
+
+/**Agregar un elemento hacie el final del arrego */
+numeros.push(11)
+
+/**Agregar un elemento hacia el principio del arreglo */
+
+numeros.unshift(-1, -2, -3)
+
+/**Eliminar el ultimo elemento del arreglo */
+
+numeros.pop();
+
+/**Eliminar el primer elemento del arreglo */
+numeros.shift()
+
+console.table(numeros)
+
+/**Rest Operator o Spread Operator */
+//agregar al final
+const NuevoArreglo = [...numeros, 20]
+
+//agregar al inicio
+//const NuevoArreglo = [-20,...numeros]
+console.table(NuevoArreglo)
