f238602b670f4edfa7f5598918176a555857401b
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-28 11:38:01 -0300

    Iteradores

---
+++ Seccion10_Javascript/js/Iteradores.js
@@ -0,0 +1,38 @@
+/**Iteradores */
+
+/**For loop */
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+
+}
+
+const auto = [
+    { Marca: 'Chevrolet', Modelo: 'sail' },
+    { Marca: 'Chevrolet', Modelo: 'spark' },
+    { Marca: 'Chevrolet', Modelo: 'spark gt' },
+    { Marca: 'Chevrolet', Modelo: 'tracker' },
+    { Marca: 'Chevrolet', Modelo: 'camaro' },
+    { Marca: 'Chevrolet', Modelo: 'captiva' },
+]
+
+for (let i = 0; i < auto.length; i++) {
+    console.log(auto[i])
+}
+
+/**While loop */
+console.log("While")
+
+let i = 0
+
+while (i <= 10) {
+    console.log(i)
+
+    i++
+}
+
+/**Do While loop */
+do {
+    console.log(i)
+
+    i++
+} while (i < 100)
