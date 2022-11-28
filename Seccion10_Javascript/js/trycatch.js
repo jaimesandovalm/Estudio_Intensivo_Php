75fb453b23d583e4e136d45d6a9e8c4577c7aa40
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-28 15:50:03 -0300

    try catch

---
+++ Seccion10_Javascript/js/trycatch.js
@@ -0,0 +1,15 @@
+/**Try catch */
+
+const numero1 = 20
+const numero2 = 30
+
+
+console.log(numero1)
+
+try {
+    console.log(numero2)
+} catch (error) {
+    console.log(error)
+}
+
+console.log(numero3)
