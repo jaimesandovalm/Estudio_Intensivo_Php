f70066b3d8c46a6f6e32da2b8de5ab86d08bbe96
jaimesandovalm <jaimesandovalm1990@gmail.com>
2022-11-29 14:44:29 -0300

    Eventos

--- Seccion11_Javascript/Documentos/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/app.js
+++ Seccion11_Javascript/Documentos/JavaScript_DOM_Eventos_inicio/JavaScript_DOM_Eventos_inicio/js/app.js
@@ -10,5 +10,27 @@ const enlaces = document.querySelectorAll('.navegacion a')
 console.log(enlaces)
 
 //getElementById
-const header2= document.getElementById()
+//const header2 = document.getElementById()
 
+//Generar nuevo enlace
+
+const nuevoEnlace = document.createElement('A')
+
+nuevoEnlace.href = 'nuevo-enlace.html'
+
+nuevoEnlace.textContent = 'Nuevo_Enlace'
+
+nuevoEnlace.classList.add('navegacion__enlace')
+
+//Eventos
+
+/**Segun el evento ejecuta un tipo de accion, mas conocido como call back */
+
+window.addEventListener('load', function () {
+    console.log(9)
+}) //este codigo se ejecuta cuando el programa esta listo
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log(7)
+})//Espera a q se descargue el HTML
